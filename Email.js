f8c6896736ed3edb285e4eddce534e9c944f2cb3
palakgu <palakjv304@gmail.com>
2025-03-05 10:52:33 +0530

    close the expression with supporting optional parts.

--- Email.js
+++ Email.js
@@ -1,7 +1,7 @@
-function validateEmail_UC4(email) {
-    const pattern = /^[a-zA-Z0-9._+-]+@[a-zA-Z]+\.[a-zA-Z]{2,}$/;
+function validateEmail_UC5(email) {
+    const pattern = /^[a-zA-Z0-9._+-]+@[a-zA-Z]+\.[a-zA-Z]{2,}(\.[a-zA-Z]{2})?$/;
     return pattern.test(email);
 }
 
-console.log(validateEmail_UC4("abc.xyz@bridgelabz.co"));  
-console.log(validateEmail_UC4("abc$xyz@bridgelabz.co"));  
+console.log(validateEmail_UC5("abc.xyz@bridgelabz.co.in")); 
+console.log(validateEmail_UC5("abc@gmail.com.1a"));        
