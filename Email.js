29c5637dc1cf27cac43cfa53ade3333489422bd0
palakgu <palakjv304@gmail.com>
2025-03-05 10:43:21 +0530

    Ensure @ and validate the mandatory 2nd part i.e. bridgelabz

--- Email.js
+++ Email.js
@@ -1,6 +1,6 @@
 //UC1 
 function validateEmail_UC1(email){
-    const pattern = /^[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]{2,}$/;
+    const pattern =  /^[a-zA-Z0-9]+@[a-zA-Z]+$/;
     return pattern.test(email);
 }
 console.log(validateEmail_UC1("abc@bridgelabz.co"));
